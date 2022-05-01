a8ba6eaff3eaa4edd1da152499776cb8062eb6d0
emirb <emirbeyazortu@gmail.com>
2022-05-02 00:37:58 +0100

    Selenium practices

--- src/test/java/com/cydeo/tests/day1_selenium_intro/BasicNavigations.java
+++ src/test/java/com/cydeo/tests/day1_selenium_intro/BasicNavigations.java
@@ -1,7 +1,64 @@
 package com.cydeo.tests.day1_selenium_intro;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class BasicNavigations {
 
-    //first one
+    public static void main(String[] args) throws InterruptedException {
+
+        //1- setup the browser driver
+        WebDriverManager.chromedriver().setup();
+
+        //2- Create instance of the driver
+        // This is the line opening an empty browser
+        WebDriver driver = new ChromeDriver();
+
+        // This line will maximize the browser
+        driver.manage().window().maximize();
+
+        //3- Go to "https://www.tesla.com"
+        driver.get("https://www.tesla.com");
+
+        // get the title of the page
+       String currentTitle = driver.getTitle();
+       System.out.println("currentTitle = " + currentTitle);
+
+
+        //Stop code execution for 3 seconds
+        Thread.sleep(3000);
+
+        // use selenium to navigate back
+        driver.navigate().back();
+
+        //Stop code execution for 3 seconds
+        Thread.sleep(3000);
+
+        // use selenium to navigate forward
+        driver.navigate().forward();
+
+        //Stop code execution for 3 seconds
+        Thread.sleep(3000);
+
+        // use selenium to navigate refresh
+        driver.navigate().refresh();
+
+        //Stop code execution for 3 seconds
+        Thread.sleep(3000);
+
+        // use navigate().to():
+        driver.navigate().to("https://www.google.com");
+        currentTitle = driver.getTitle();
+        System.out.println("currentTitle = " + currentTitle);
+
+        // Get the current URL using Selenium
+        String currentURL = driver.getCurrentUrl();
+
+        System.out.println("currentURL = " + currentURL);
+
+
+
+    }
 
 }
